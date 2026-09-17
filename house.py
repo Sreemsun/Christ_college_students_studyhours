c3241ef1f952e69067dde5d290117cc56b2e7b33
Sreemsun <sreemsun21anand@gmail.com>
2026-09-17 14:16:13 +0530

    Update house.py feature names to match new dataset (Area, Total_Floors, Bedrooms)

--- house.py
+++ house.py
@@ -11,22 +11,21 @@ st.title("House Price Predictor")
 st.write("Enter the details of the property below to predict the estimated price.")
 
 area = st.number_input("Area (sq ft)", min_value=100.0, max_value=20000.0, value=1200.0, step=50.0)
-floors = st.number_input("Total_Floors", min_value=0, max_value=100, value=5, step=1)
-bedroom = st.number_input("Bedrooms", min_value=1, max_value=20, value=3, step=1)
-
+floors = st.number_input("Total Floors", min_value=1, max_value=100, value=2, step=1)
+bedrooms = st.number_input("Bedrooms", min_value=1, max_value=20, value=3, step=1)
 
 if st.button("Predict"):
 	if area > 6000:
 		st.error("Error: Area cannot be greater than 6000 sq ft.")
-	elif bedroom > 5:
+	elif bedrooms > 5:
 		st.error("Error: Number of bedrooms cannot be greater than 5.")
 	elif floors > 10:
 		st.error("Error: Number of floors cannot be greater than 10.")
 	else:
 		input_data = pd.DataFrame({
 			"Area": [area],
-			"Bedroom": [bedroom],
-			"Total_Floors": [floors]
+			"Total_Floors": [floors],
+			"Bedrooms": [bedrooms]
 		})
 
 		prediction = model.predict(input_data)[0]
