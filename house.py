994995547c17a3598f28ae48374c9900f1c53638
Sreemsun <sreemsun21anand@gmail.com>
2026-09-17 14:14:06 +0530

    NN

--- house.py
+++ house.py
@@ -11,21 +11,22 @@ st.title("House Price Predictor")
 st.write("Enter the details of the property below to predict the estimated price.")
 
 area = st.number_input("Area (sq ft)", min_value=100.0, max_value=20000.0, value=1200.0, step=50.0)
+floors = st.number_input("Total_Floors", min_value=0, max_value=100, value=5, step=1)
 bedroom = st.number_input("Bedrooms", min_value=1, max_value=20, value=3, step=1)
-age = st.number_input("Age of House (years)", min_value=0, max_value=100, value=5, step=1)
+
 
 if st.button("Predict"):
 	if area > 6000:
 		st.error("Error: Area cannot be greater than 6000 sq ft.")
 	elif bedroom > 5:
 		st.error("Error: Number of bedrooms cannot be greater than 5.")
-	elif age > 10:
-		st.error("Error: Age of house cannot be greater than 10 years.")
+	elif floors > 10:
+		st.error("Error: Number of floors cannot be greater than 10.")
 	else:
 		input_data = pd.DataFrame({
 			"Area": [area],
 			"Bedroom": [bedroom],
-			"Age": [age]
+			"Total_Floors": [floors]
 		})
 
 		prediction = model.predict(input_data)[0]
