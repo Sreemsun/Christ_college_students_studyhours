0bb3d41e952226047b513e7dc4cfa70dc071d983
Sreemsun <sreemsun21anand@gmail.com>
2026-09-17 12:31:40 +0530

    Update House_price_model.pkl to LinearRegression to fix fixed-class predictions

--- house.py
+++ house.py
@@ -30,4 +30,4 @@ if st.button("Predict Price", type="primary"):
     })
     
     prediction = model.predict(input_data)[0]
-    st.success(f"### Estimated Price: {prediction} Lakhs")
+    st.success(f"### Estimated Price: {prediction:.2f} Lakhs")
